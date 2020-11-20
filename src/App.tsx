595befad78a2d24a0b8a95e161b449e5d65a392d
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-11-20 13:33:58 -0300

    chore(eslint): successfully added typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      {/* eslint-disable-next-line */}
-      <div onClick={() => {}}></div>
-    </div>
-  );
+const func = (arg: boolean): boolean => {
+  if (arg) {
+    return true;
+  }
+  return false;
 };
+
+func(true);
+
+//eslint-disable-next-line
+const App = () => (
+  <div>
+    {/* eslint-disable-next-line */}
+    <div onClick={() => {}}></div>
+  </div>
+);
 
 export default App;
