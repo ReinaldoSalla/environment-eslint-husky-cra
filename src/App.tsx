2bef429ea688c9e14ef0995571acf2f2668688e2
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-11-24 09:13:56 -0300

    chore(eslint): exposing an conflict for "implicit-arrow-linebreak": "error"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 
 const func = (arg: boolean): boolean => {
@@ -9,11 +10,24 @@ const func = (arg: boolean): boolean => {
 
 func(true);
 
+// on this section, prettier and eslint would conflict if eslint uses this rule
+// "implicit-arrow-linebreak": "error"
+
+// const func2 = (arg: boolean): string => (
+//   arg
+//     ? 'arg is truessssssssssssssssssssssssssssssssssssssssssssssssssssssss'
+//     : 'arg is not truexxxxxxxxxxxxxxxxxxxddddddd';
+// )
+
+// func2(true);
+
 // eslint-disable-next-line
 const App = () => (
   <div>
     {/* eslint-disable-next-line */}
     <div onClick={() => {}} />
+    {/* eslint-disable-next-line */}
+    <button type='button'>cliclk</button>
   </div>
 );
 
